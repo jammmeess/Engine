893c53799e0a9f06cdde0bee73c489f5bbf6d0f2
jammmeess <james_bomediano@hotmail.com>
2023-07-12 00:01:52 +0800

    Minor Changes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,3 +6,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 <!-- <script src="/js/hideNotif.js"></script> -->
 <link rel="stylesheet" href="/css/styles.css" />
+<script src="{{ asset('js/navbar.js') }}"></script>
