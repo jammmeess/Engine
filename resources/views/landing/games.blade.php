3ea5097f93f246a151259148259271bd087e09a6
jammmeess <james_bomediano@hotmail.com>
2023-07-13 23:13:48 +0800

    Added Landing Folder

---
+++ resources/views/landing/games.blade.php
@@ -0,0 +1,173 @@
+        <div class="container-fluid px-5" id="Games">
+            <div class="col-lg-12 col-12 d-none d-sm-block">
+                <h3 class="display-3 text-light d-flex justify-content-center">Our Games</h3>
+                <div class="line mb-5 d-flex justify-content-center"></div>
+            </div>
+            <!-- Normal Screen -->
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-12 ps-5 pt-5  d-none d-sm-block">
+                    <div id="carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3500">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner img-fluid" style="width: 100%">
+                            <div class="carousel-item active">
+                                <img src="/img/landing/Valorant.gif" class="d-block w-100" alt="Valorant">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/img/landing/rdr2.gif" class="d-block w-100" alt="Red Dead Redemption 2">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/img/landing/spider-man.gif" class="d-block w-100" alt="New World">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4  my-5 d-none d-sm-block">
+                        <div class="card custom-card">
+                            <img src="/img/landing/Valorant.jpg" class="card-img-top" alt="Valorant">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4  my-5 d-none d-sm-block">
+                        <div class="card custom-card">
+                            <img src="/img/landing/rdr2.jpg" class="card-img-top" alt="Red Dead Redemption II">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4  my-5 d-none d-sm-block">
+                        <div class="card custom-card">
+                            <img src="/img/landing/spider-man.jpg" class="card-img-top" alt="Marvel's Spider-Man Remastered">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12  d-none d-sm-block">
+                        <h3 class="text-center text-light display-6">Discover New Games with Engine_ </h3>
+                        <div class="line"></div>
+                    </div>
+
+                    <div class="col-lg-4  mt-4 d-none d-sm-block">
+                        <div class="card custom-card">
+                            <img src="/img/landing/newworld.jpg" class="card-img-top" alt="New World">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4  mt-4 d-none d-sm-block">
+                        <div class="card custom-card">
+                            <img src="/img/landing/Stray.jpg" class="card-img-top" alt="Stray">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4  mt-4 mb-5 d-none d-sm-block">
+                        <div class="card custom-card">
+                            <img src="/img/landing/ugg.jpg" class="card-img-top" alt="Untitled Goose Game">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12  d-none d-sm-block">
+                        <div class="d-flex justify-content-center mb-5">
+                            <button class="button text-center" style="font-family: Montserrat">Shop Now</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- Responsive -->
+        <div class="container-fluid px-5 d-block d-sm-none" id="Games-responsive">
+            <div class="col-lg-12 col-12 d-block d-sm-none">
+                <h3 class="display-3 text-light d-flex justify-content-center">Our Games</h3>
+                <div class="line mb-5 d-flex justify-content-center"></div>
+            </div>
+
+            <div class="col-12  pt-2 d-block d-sm-none">
+                <div id="carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3500">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner img-fluid" style="width: 100%">
+                        <div class="carousel-item active">
+                            <img src="/img/landing/Valorant.gif" class="d-block w-100" alt="Valorant">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/img/landing/rdr2.gif" class="d-block w-100" alt="Red Dead Redemption 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/img/landing/spider-man.gif" class="d-block w-100" alt="New World">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 my-2 d-block d-sm-none">
+                    <div class="card custom-card">
+                        <img src="/img/landing/Valorant.jpg" class="card-img-top" alt="Valorant">
+                    </div>
+                </div>
+
+                <div class="col-12 my-2 d-block d-sm-none">
+                    <div class="card custom-card">
+                        <img src="/img/landing/rdr2.jpg" class="card-img-top" alt="Red Dead Redemption II">
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2 mb-3 d-block d-sm-none">
+                    <div class="card custom-card">
+                        <img src="/img/landing/spider-man.jpg" class="card-img-top" alt="Marvel's Spider-Man Remastered">
+                    </div>
+                </div>
+
+                <div class="col-12 d-block d-sm-none">
+                    <h3 class="text-center text-light display-6">Discover New Games with Engine_ </h3>
+                </div>
+
+                <div class="col-12 my-2 d-block d-sm-none">
+                    <div class="card custom-card">
+                        <img src="/img/landing/newworld.jpg" class="card-img-top" alt="New World">
+                    </div>
+                </div>
+
+                <div class="col-12 my-2 d-block d-sm-none">
+                    <div class="card custom-card">
+                        <img src="/img/landing/Stray.jpg" class="card-img-top" alt="Stray">
+                    </div>
+                </div>
+
+                <div class="col-12 my-2 d-block d-sm-none">
+                    <div class="card custom-card">
+                        <img src="/img/landing/ugg.jpg" class="card-img-top" alt="Untitled Goose Game">
+                    </div>
+                </div>
+
+                <div class="col-12 d-block d-sm-none">
+                    <div class="d-flex justify-content-center my-4">
+                        <button class="button text-center" style="font-family: Montserrat">Shop Now</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
---
+++ resources/views/landing/hero.blade.php
@@ -0,0 +1,16 @@
+<div class="hero">
+    <div class="overlay" id="overlay"></div>
+    <img src="/img/landing/HeroBanner.gif" id="hero" />
+    <div class="content" id="content">
+        <div class="container">
+            <div class="row mx-5 mt-5">
+                <div class="col-lg-6 col-12">
+                    <h1 class="fw-bolder display-1">WELCOME TO ENGINE_</h1>
+                </div>
+                <div class="col-lg-12 col-12 mt-4">
+                    <button class="button text-center" style="font-family: Montserrat">Press Start</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
