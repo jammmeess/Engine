b9f1538075b31b68ea47ce2eb2763483e4f4d730
jammmeess <james_bomediano@hotmail.com>
2023-07-13 23:14:55 +0800

    Added Footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,80 @@
+<footer class="footer-section">
+    <div class="container-fluid px-5 py-5">
+        <div class="footer-content pt-5 pb-5">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html"><img src="/img/logo/logo_white_main.png" class="img-fluid" alt="logo"></a>
+                        </div>
+                        <div class="footer-text">
+                            <p>Engine_ Powering the Gaming Revolution.
+                                © 2023 Engine_ Games. All rights reserved.
+                                Dona Soledad Avenue, Paranaque, PH | Phone: +63 9123456789| Email: engine_games@engine.com
+                            </p>
+                        </div>
+                        <div class="footer-social-icon">
+                            <span>Follow us</span>
+                            <a href="https://www.facebook.com"><img src="/img/icons/facebook.png" style="width: 30px"></a>
+                            <a href="https://www.twitter.com"><img src="/img/icons/twitter.png" style="width: 30px"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>Links</h3>
+                        </div>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#About">About</a></li>
+                            <li><a href="#Games">Games</a></li>
+                            <li><a href="#">Faqs</a></li>
+                            <li><a href="#">Support</a></li>
+                            <li><a href="#">Store</a></li>
+                            <li><a href="#">Latest News</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>Subscribe</h3>
+                        </div>
+                        <div class="footer-text mb-25">
+                            <p>Don't miss to subscribe to our new feeds, kindly fill the form below.</p>
+                        </div>
+                        <div class="subscribe-form">
+                            <form action="#">
+                                <input type="text" placeholder="Email Address">
+                                <button><img src="/img/icons/send.png" style="width: 30px"></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                    <div class="copyright-text">
+                        <p>Copyright &copy; 2018, All Right Reserved <a href="#">Engine_</a></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                    <div class="footer-menu">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Terms</a></li>
+                            <li><a href="#">Privacy</a></li>
+                            <li><a href="#">Policy</a></li>
+                            <li><a href="#">Support</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
