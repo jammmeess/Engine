e9fdf01243c33afd8545db11ac1e9092ccf47088
jammmeess <james_bomediano@hotmail.com>
2023-07-14 20:45:42 +0800

    Responsive Feature, Updated Button, Updated Font Style with Glitch effect

--- resources/views/landing/hero.blade.php
+++ resources/views/landing/hero.blade.php
@@ -1,15 +1,35 @@
-<div class="hero">
+<div class="hero col-lg-12 col-12">
     <div class="overlay" id="overlay"></div>
-    <img src="/img/landing/HeroBanner.gif" id="hero" />
+    <img src="/img/landing/HeroBanner.gif" id="hero" class="img-fluid" />
     <div class="content" id="content">
         <div class="container">
-            <div class="row mx-5 mt-5">
-                <div class="col-lg-6 col-12">
-                    <h1 class="fw-bolder display-1">WELCOME TO ENGINE_</h1>
+            <div class="row mx-5 mt-5 top-0">
+
+                <div class="col-lg-6 col-12 d-none d-sm-block ">
+                    <div class="glitch-wrapper">
+                        <h1 class="fw-bolder display-1">
+                            <div class="glitch" data-text="IGNITE PASSION_">IGNITE PASSION_</div>
+                        </h1>
+                    </div>
                 </div>
-                <div class="col-lg-12 col-12 mt-4">
-                    <button class="button text-center" style="font-family: Montserrat">Press Start</button>
+
+                <div class="col-lg-12 d-none d-sm-block ">
+                    <button class="btn text-start" style="font-family: Helmet">Press Start</button>
                 </div>
+
+                <!-- Responsive -->
+                <div class="col-12 d-block d-sm-none">
+                    <div class="glitch-wrapper-responsive">
+                        <h1 class="fw-bolder">
+                            <div class="glitch-responsive" data-text="IGNITE PASSION_">IGNITE PASSION_</div>
+                        </h1>
+                    </div>
+                </div>
+
+                <div class="col-12 d-block d-sm-none">
+                    <button class="btn text-center" style="font-family: Helmet">Press Start</button>
+                </div>
+
             </div>
         </div>
     </div>
