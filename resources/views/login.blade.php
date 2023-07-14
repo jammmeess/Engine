bb2588d65f4c39091b4e6a47cf6ec766cda88d7b
jammmeess <james_bomediano@hotmail.com>
2023-07-14 15:00:18 +0800

    @csrf Form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,6 +23,7 @@
 
             <div class="col-lg-12 d-flex justify-content-center">
                 <form id="form">
+                    @csrf
                     <div class="mb-3 mt-3">
                         <label for="exampleInputEmail1" class="form-label text-light">Email: </label>
                         <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp">
