fef4be53cd079d10bf7489a5473bffbf5f154758
jammmeess <james_bomediano@hotmail.com>
2023-07-14 20:47:29 +0800

    Login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,43 +9,63 @@
 
 <body>
     @include("layouts/navbar")
-    <div class="overlay-login" id="overlay-login"></div>
-    <div class="container">
-        <div class="row">
+    <section>
+        <div class="container">
+            <div class="row" id="login-bg">
 
-            <div class="col-lg-12 d-flex justify-content-center">
-                <h3 class="text-light display-6" id="login-title">Welcome to Engine_</h3>
+                <div class="col-lg-6  d-none d-sm-block mx-5 mt-2 ">
+                    <img src="/img/logo/logo_black_main.png" class="img-fluid my-3" style="width: 70%">
+                    <form>
+                        @csrf
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email: </label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+                        <div class="form-group my-3">
+                            <label for="exampleInputPassword1">Password:</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                        </div>
+                        <a href="/"><button type="submit" class="btn mt-3">Cancel</button></a>
+                        <button type="submit" class="btn mt-3">Submit</button>
+                    </form>
+                    <p>Not yet a member?</p> <a><button type="submit" class="btn mb-5">Register Here</button></a>
+                </div>
+
+                <!-- Responsive -->
+                <div class="col-11 d-block d-sm-none mx-3 mt-2 ">
+                    <img src="/img/logo/logo_black_main.png" class="img-fluid my-3" style="width: 70%">
+                    <form>
+                        @csrf
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email: </label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+                        <div class="form-group my-3">
+                            <label for="exampleInputPassword1">Password:</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                        </div>
+                        <a href="/" type="submit" class="btn mt-3">Cancel</a>
+                        <button type="submit" class="btn mt-3">Submit</button>
+                    </form>
+                    <p>Not yet a member?</p> <a><button type="submit" class="btn mb-5">Register Here</button></a>
+                </div>
+
+                <div class="col-lg-4 d-flex justify-content-center align-items-center">
+                    <img src="/img/landing/symmetra.gif" id="login_img" style="height: 90%; border: 10px solid">
+                </div>
+
             </div>
-            <div class="line"></div>
-            <div class="col-lg-12 d-flex justify-content-center">
-                <h3 class="text-light mt-5" id="login-sub">Log in to your Account</h3>
-            </div>
-
-            <div class="col-lg-12 d-flex justify-content-center">
-                <form id="form">
-                    @csrf
-                    <div class="mb-3 mt-3">
-                        <label for="exampleInputEmail1" class="form-label text-light">Email: </label>
-                        <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label text-light">Password: </label>
-                        <input type="password " class="form-control">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label text-light" for="exampleCheck1">Keep me signed in</label>
-                    </div>
-                    <button type="submit" class="button" style="font-family: Vanilla; width: 200px; height: 55px; font-size: 20px">Submit</button>
-                </form>
-            </div>
-
-
         </div>
-    </div>
+    </section>
     @include('layouts/footer')
 </body>
-
 
 </html>
