f96d40241745dbafb52117c96dc24f72ef1a991d
jammmeess <james_bomediano@hotmail.com>
2023-07-17 22:33:32 +0800

    Hide Notif Script

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,6 +4,6 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<!-- <script src="/js/hideNotif.js"></script> -->
+<script src="/js/hideNotif.js"></script>
 <link rel="stylesheet" href="/css/styles.css" />
 <script src="{{ asset('js/navbar.js') }}"></script>
