8d3bc43f4b7fc646022ac6c711fa43f94082c8f9
jammmeess <james_bomediano@hotmail.com>
2023-07-17 20:26:47 +0800

    New Routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 ////////////PUBLIC ///////////
-Route::get("/", function () {
-    return view("home");
-});
+Route::get('/', [UserController::class, 'showHome']);
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', [UserController::class, 'showLogin']);
+
+//////////// USER ///////////
+
+
+//////////// ADMIN ///////////
+Route::get('/login/register', [UserController::class, 'showRegister']);
+Route::post('/login/register/success', [UserController::class, 'register']);
+Route::get('/login/register/success', [UserController::class, 'showRegisterSuccess']);
