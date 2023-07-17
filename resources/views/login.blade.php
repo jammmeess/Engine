6f88aa54137ce175cc14630b1d2e81705018b391
jammmeess <james_bomediano@hotmail.com>
2023-07-17 22:34:59 +0800

    Login Errors Functionality, Temporarily Removed Remember me button

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,30 +7,32 @@
     <title>Log In</title>
 </head>
 
-<body>
+<body> 
+    @include("layouts/errors")
     @include("layouts/navbar")
+
     <section>
         <div class="container">
             <div class="row" id="login-bg">
 
                 <div class="col-lg-6  d-none d-sm-block mx-5 mt-2 ">
                     <img src="/img/logo/logo_black_main.png" class="img-fluid my-3" style="width: 70%">
-                    <form>
+                    <form action="/login" method="POST">
                         @csrf
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email: </label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <label for="email">Email: </label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
                         </div>
                         <div class="form-group my-3">
-                            <label for="exampleInputPassword1">Password:</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            <label for="password">Password:</label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
                         </div>
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
-                        </div>
+                        <!-- <div class="form-check">
+                            <input type="checkbox" class="form-check-input">
+                            <label class="form-check-label" >Remember Me</label>
+                        </div> -->
                         <a href="/" type="button" class="btn mt-3">Cancel</></a>
-                        <button type="submit" class="btn mt-3">Submit</button>
+                        <button type="submit" class="btn mt-3">Log In</button>
                     </form>
                     <p>Not yet a member?</p> <a href="/login/register"><button type="button" class="btn mb-5">Register Here</button></a>
                 </div>
@@ -38,22 +40,22 @@
                 <!-- Responsive -->
                 <div class="col-11 d-block d-sm-none mx-3 mt-2 ">
                     <img src="/img/logo/logo_black_main.png" class="img-fluid my-3" style="width: 70%">
-                    <form>
+                    <form action="/login" method="POST">
                         @csrf
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email: </label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <label for="email">Email: </label>
+                            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
                         </div>
                         <div class="form-group my-3">
-                            <label for="exampleInputPassword1">Password:</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            <label for="password">Password:</label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
                         </div>
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Remember Me</label>
-                        </div>
+                        <!-- <div class="form-check">
+                            <input type="checkbox" class="form-check-input" >
+                            <label class="form-check-label">Remember Me</label>
+                        </div> -->
                         <a href="/" type="submit" class="btn mt-3">Cancel</a>
-                        <button type="submit" class="btn mt-3">Submit</button>
+                        <button type="submit" class="btn mt-3">Log In</button>
                     </form>
                     <p>Not yet a member?</p> <a href="/login/register"><button type="button" class="btn mb-5">Register Here</button></a>
                 </div>
