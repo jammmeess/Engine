b7e1c0ac511ba52126586ddcf4087b4e8536670e
jammmeess <james_bomediano@hotmail.com>
2023-07-18 11:33:16 +0800

    Support code

---
+++ resources/views/support.blade.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    @include("layouts/head")
+    <link rel="stylesheet" href="/css/styles.css" />
+    <title>Support</title>
+</head>
+
+<body>
+    @include("layouts/navbar")
+    <div class="container-fluid mx-5 py-5 my-5" id="support">
+        <div class="row">
+            <div class="col-lg-12 text-light">
+                <h2>Engine_ Support</h2>
+                <h3>What do you need help with?</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12 text-light mt-5">
+                <p>Please Sign in to your account to review purchases, account status, and get personalized help.</p>
+                <div class="col-lg-8">
+                    <a class="btn mb-5 me-3" href="/login">Log in to Engine_ </a>
+                    <a class="btn mb-5" href="/register">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('layouts/footer')
+</body>
+
+</html>
