58a2dcbf7df36ac058fa61445dd92724669984a4
jammmeess <james_bomediano@hotmail.com>
2023-07-18 10:52:34 +0800

    FAQS

---
+++ resources/views/landing/faqs.blade.php
@@ -0,0 +1,117 @@
+<div class="row py-5">
+    <div class="col-lg-12 d-none d-sm-block" id="Faqs">
+        <h3 class="mt-5 display-3 text-light d-flex justify-content-center">FAQS</h3>
+        <div class="line"></div>
+
+        <div class="accordion px-5 py-5">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        1. What is Engine_?
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        Engine_ is an online gaming e-commerce website that offers a wide range of gaming products, including downloadable video games, consoles, accessories, and merchandise. We aim to provide gamers with a convenient platform to explore, purchase, and enjoy the latest gaming experiences.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        2. How can I make a purchase on Engine_?
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        Making a purchase on Engine_ is easy. Simply browse our website, add desired items to your cart, proceed to checkout, and follow the instructions to complete your order. You will need to create an account to make a purchase.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        3. Are all games downloadable upon purchase?
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        Yes, all games listed on Engine_ are available for download upon purchase. Once your payment is confirmed, you'll receive a unique download code or a link to access your game. This allows you to start playing your purchased game without the need for physical media.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFour">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        4. What payment methods do you accept?
+                    </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        We accept a variety of payment methods, including credit/debit cards, PayPal, and other popular online payment gateways. During the checkout process, you'll be able to choose the payment method that suits you best.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFive">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                        5. How long does the download process take?
+                    </button>
+                </h2>
+                <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        The download process depends on the size of the game and your internet connection speed. Generally, it can take anywhere from a few minutes to several hours. Factors such as the game's size, your internet speed, and network congestion can influence the download time.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSix">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                        6. Can I re-download my purchased games?
+                    </button>
+                </h2>
+                <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        Yes, if you need to re-download a game you purchased from Engine_, you can do so. Your account will typically have a "Library" or "Purchased Games" section where you can access and download your previously purchased games. This allows you to reinstall games on different devices or in case you need to reinstall them.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSeven">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                        7. Can I get a refund for a downloaded game?
+                    </button>
+                </h2>
+                <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        Refund policies for downloaded games may vary. Please refer to our Returns and Refunds Policy on our website to understand the specific conditions and eligibility criteria for refunds. In some cases, once a game has been downloaded or its activation code has been used, refunds may not be possible.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingEight">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                        8. How can I contact customer support?
+                    </button>
+                </h2>
+                <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        Our dedicated customer support team is available to assist you with any queries or concerns. You can reach us through our contact form on the website, or you can find our customer support email and phone number in the "Contact Us" section. We strive to respond to all inquiries promptly.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingNIne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNIne" aria-expanded="false" aria-controls="collapseNIne">
+                        9. Can I cancel or modify my order?
+                    </button>
+                </h2>
+                <div id="collapseNIne" class="accordion-collapse collapse" aria-labelledby="headingNIne" data-bs-parent="#faqs">
+                    <div class="accordion-body">
+                        If you need to cancel or modify your order, please reach out to our customer support as soon as possible. We will do our best to accommodate your request, but please note that once a game has been downloaded or an activation code has been used, cancellations or modifications may not be possible.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
