90ff0422cc6ecf84929a0fe7aeaae569a49937fb
jammmeess <james_bomediano@hotmail.com>
2023-07-18 11:32:17 +0800

    Added Faqs

--- resources/views/userhome.blade.php
+++ resources/views/userhome.blade.php
@@ -15,6 +15,8 @@
         @include('landing/about')
         <!-- Games -->
         @include('landing/games')
+        <!-- Faqs -->
+        @include('landing/faqs')
         <!-- footer -->
         @include("layouts/footer")
     </section>
