35b38a8db581700d9567a022d4f813cbb8cd3faf
jammmeess <james_bomediano@hotmail.com>
2023-07-18 16:52:27 +0800

    Updated Success page

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -10,15 +10,26 @@
     <section>
         @include("layouts/navbar")
 
-        <div class="container">
+        <div class="container d-none d-sm-block">
             <div class="row px-5 py-5" id="success">
-                <div class="col-lg-12">
+                <div class="col-lg-12 text-light">
                     <h3>Registration Success!</h3>
                     <p> Click the link below to Proceed to the Log in page</p>
                     <a href='/login' class="btn">To Login Page</a>
                 </div>
             </div>
         </div>
+        <!-- Responsive -->
+        <div class="container d-block d-sm-none">
+            <div class="row" id="success-responsive">
+                <div class="col-12 text-light">
+                    <h3>Registration Success!</h3>
+                    <p> Click the link below to Proceed to the Log in page</p>
+                    <a href='/login' class="btn">To Login Page</a>
+                </div>
+            </div>
+        </div>
+
         @include("layouts/footer")
     </section>
 </body>
