22058e4b2fc52b2d4313530f2b768b61d0546a91
jammmeess <james_bomediano@hotmail.com>
2023-07-18 14:42:36 +0800

    Contact code

---
+++ resources/views/layouts/contact.blade.php
@@ -0,0 +1,50 @@
+<div class="container-fluid">
+    <h3 id="contact_us" class="col-lg-12 col-12 pt-5 text-start ">
+        Contact Us.
+    </h3>
+    <h5>
+        Please feel free to contact us if you need any further information on
+        Library.
+    </h5>
+    <div class="row d-flex justify-content-start align-items-center">
+
+        <div class="col-lg-5 col-12 mt-2 pt-2">
+            <form>
+                <div class="form-group pt-3 row">
+                    <label for="enter_name" class="col-sm-2 col-form-label" placeholder="ex. Juan Dela Cruz">Name:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="enter_name" />
+                    </div>
+                </div>
+
+                <div class="form-group pt-3 row">
+                    <label for="contact_number" class="col-sm-2 col-form-label" placeholder="ex. 09123456789">Contact Number:</label>
+                    <div class="col-sm-10">
+                        <input type="Number" class="form-control" id="contact_number" />
+                    </div>
+                </div>
+
+                <div class="form-group pt-3 row">
+                    <label for="message" class="col-sm-2 col-form-label" placeholder="Type your message here...">Message:</label>
+                    <div class="col-lg-10">
+                        <input type="text" class="form-control" id="message" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <p class="pt-5">Contact:</p>
+                    <span>engine_games@engine.com | +6391234567890</span>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4 col-2 me-1">
+                        <button type="button" class="btn btn-secondary my-5">
+                            Send
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
