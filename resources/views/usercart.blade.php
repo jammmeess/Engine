930d545367725eaff680d1302c2560f8e822e76b
jammmeess <james_bomediano@hotmail.com>
2023-07-19 23:58:59 +0800

    User Cart

---
+++ resources/views/usercart.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+
+<head>
+    @include("layouts/head")
+    <title>Cart</title>
+</head>
+
+<body>
+    @include('layouts/navbar')
+    @include('layouts/footer')
+</body>
+
+</html>
