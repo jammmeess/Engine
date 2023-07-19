5bd6d77915356c77b085b1f46fc3388466f71a14
jammmeess <james_bomediano@hotmail.com>
2023-07-19 16:53:07 +0800

    Revamped the navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,26 +1,26 @@
 @include('layouts/app')
 <nav class="navbar navbar-expand-lg navbar-custom">
   <div class="container-fluid">
-    <a class="navbar-brand mx-4" href="/"><img src="/img/logo/logo_white_main.png" style="width: 100px" /></a>
+    @if(Session::has('user_id'))
+    <a class="navbar-brand mx-4" href="/userhome"><img src="/img/logo/logo_white_main.png" style="width: 100px" /></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <img src="/img/icons/toggle.png" style="width: 30px">
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="/#About">About</a>
+          <a class="nav-link  text-light ms-2" href="/userhome#About">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="/#Games">Games</a>
+          <a class="nav-link  text-light ms-2" href="/userhome#Games">Games</a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="/#Faqs">FAQS</a>
+          <a class="nav-link  text-light ms-2" href="/userhome#Faqs">FAQS</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="#Store">Store</a>
+          <a class="nav-link  text-light ms-2" href="/userstore">Store</a>
         </li>
-        @if (Session::has('user_id'))
         <li class=" nav-item">
           <a class="nav-link  text-light" href="/usersupport">Support</a>
         </li>
@@ -40,15 +40,34 @@
             <li><a class="dropdown-item" href="/logout">Logout</a></li>
         </li>
         @else
-        <li class=" nav-item">
-          <a class="nav-link  text-light" href="/support">Support</a>
-        </li>
-        <a href="/login"><button type="button" class="btn mx-3">
-            Log In / Sign Up
-          </button></a>
-        @endif
+        <a class="navbar-brand mx-4" href="/"><img src="/img/logo/logo_white_main.png" style="width: 100px" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <img src="/img/icons/toggle.png" style="width: 30px">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link  text-light ms-2" href="/#About">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  text-light ms-2" href="/#Games">Games</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link  text-light ms-2" href="/#Faqs">FAQS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  text-light ms-2" href="/store">Store</a>
+            </li>
+            <li class=" nav-item">
+              <a class="nav-link  text-light" href="/support">Support</a>
+            </li>
+            <a href="/login"><button type="button" class="btn mx-3">
+                Log In / Sign Up
+              </button></a>
+            @endif
 
 
+        </div>
     </div>
-  </div>
 </nav>
