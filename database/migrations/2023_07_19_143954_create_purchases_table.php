fc8fb6b641570ee340f8f2cd6c2cea44ea5e5ab5
jammmeess <james_bomediano@hotmail.com>
2023-07-19 23:57:55 +0800

    Database Migrations

---
+++ database/migrations/2023_07_19_143954_create_purchases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->integer('purchase_id');
+            $table->timestamp('time_placed')->useCurrent();
+            $table->integer('game_id');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
