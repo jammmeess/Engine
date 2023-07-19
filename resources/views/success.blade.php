1ea7d54e594b24d1de5f9e0e17f52d8b4d064d40
jammmeess <james_bomediano@hotmail.com>
2023-07-19 23:55:12 +0800

    Updated Success Page

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -10,7 +10,7 @@
     <section>
         @include("layouts/navbar")
 
-        <div class="container d-none d-sm-block">
+        <div class="container-fluid d-none d-sm-block mx-5 my-5">
             <div class="row px-5 py-5" id="success">
                 <div class="col-lg-12 text-light">
                     <h3>Registration Success!</h3>
@@ -20,7 +20,7 @@
             </div>
         </div>
         <!-- Responsive -->
-        <div class="container d-block d-sm-none">
+        <div class="container-fluid d-block d-sm-none mx-2 my-5">
             <div class="row" id="success-responsive">
                 <div class="col-12 text-light">
                     <h3>Registration Success!</h3>
@@ -29,7 +29,6 @@
                 </div>
             </div>
         </div>
-
         @include("layouts/footer")
     </section>
 </body>
