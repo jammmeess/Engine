8f9fe9f90737d954fe44071b4779dcddf7bd9ce6
jammmeess <james_bomediano@hotmail.com>
2023-07-19 16:53:40 +0800

    Updated Profile Picture Size

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,29 +9,32 @@
     @include('layouts/navbar')
     <div class="container d-none d-sm-block">
         <div class="row d-flex justify-content-center">
-            <div class="col-lg-12">
-                <div class="text-start">
-                    <div class="row align-items-center">
-                        <div class="col-lg-6">
-                            <img src="/img/user_profiles/{{$u -> image}}" id="display-photo" /><br>
+            <div class="col-lg-12 mt-5">
+                <div class="row align-items-center">
+                    <div class="col-lg-3">
+                        <img src="/img/user_profiles/{{$u -> image}}" class="img-fluid" id="display-photo" /><br>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="glitch-wrapper">
+                            <h4>
+                                <div class="glitch userprofile" data-text="Welcome, {{$u->username}}!">Welcome, {{$u->username}}!</div>
+                            </h4>
                         </div>
-                        <div class="col-lg-6 ">
-                            <div class="glitch-wrapper">
-                                <h4>
-                                    <div class="glitch userprofile" data-text="Welcome, {{$u->username}}!">Welcome, {{$u->username}}!</div>
-                                </h4>
-                            </div>
-                            <h5 class="text-light"><i>{{$u->first_name}} {{$u->last_name}}</i></h5>
-                            <h5 class="text-light"><i>{{$u->birthday}}</i></h5>
-                            <button type="button" class="btn mt-3" data-bs-toggle="modal" data-bs-target="#uploadModal">
-                                Upload Photo
-                            </button>
-                        </div>
+                        <h5 class="text-light"><i>{{$u->first_name}} {{$u->last_name}}</i></h5>
+                        <h5 class="text-light"><i>{{$u->birthday}}</i></h5>
+                        <button type="button" class="btn mt-3" data-bs-toggle="modal" data-bs-target="#uploadModal">
+                            Upload Photo
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="row">
+            <h3 class="text-light text-center mt-5 pt-5">Games you own</h3>
+            <div class="line mt-3"></div>
+        </div>
     </div>
+
 
     <!-- Responsive -->
     <div class="container d-block d-sm-none">
