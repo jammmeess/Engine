aaa3501d4f7daef3eac10023d3ec9c99c2b93186
jammmeess <james_bomediano@hotmail.com>
2023-07-19 23:54:00 +0800

    Minor Changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,64 +1,63 @@
 <footer class="footer-section">
     <div class="container-fluid px-5 py-5">
-        <div class="footer-content pt-5 pb-5">
-            <div class="row">
-                <div class="col-xl-4 col-lg-4 mb-50">
-                    <div class="footer-widget">
-                        <div class="footer-logo">
-                            <a href="index.html"><img src="/img/logo/logo_white_main.png" class="img-fluid" alt="logo"></a>
-                        </div>
-                        <div class="footer-text">
-                            <p>Engine_ Powering the Gaming Revolution.
-                                © 2023 Engine_ Games. All rights reserved.
-                                Dona Soledad Avenue, Paranaque, PH | Phone: +63 9123456789| Email: engine_games@engine.com
-                            </p>
-                        </div>
-                        <div class="footer-social-icon">
-                            <span>Follow us</span>
-                            <a href="https://www.facebook.com"><img src="/img/icons/facebook.png" style="width: 30px"></a>
-                            <a href="https://www.twitter.com"><img src="/img/icons/twitter.png" style="width: 30px"></a>
-                        </div>
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 mb-50">
+                <div class="footer-widget">
+                    <div class="footer-logo">
+                        <a href="index.html"><img src="/img/logo/logo_white_main.png" class="img-fluid" alt="logo"></a>
+                    </div>
+                    <div class="footer-text">
+                        <p>Engine_ Powering the Gaming Revolution.
+                            © 2023 Engine_ Games. All rights reserved.
+                            Dona Soledad Avenue, Paranaque, PH | Phone: +63 9123456789| Email: engine_games@engine.com
+                        </p>
+                    </div>
+                    <div class="footer-social-icon">
+                        <span>Follow us</span>
+                        <a href="https://www.facebook.com"><img src="/img/icons/facebook.png" style="width: 30px"></a>
+                        <a href="https://www.twitter.com"><img src="/img/icons/twitter.png" style="width: 30px"></a>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
-                    <div class="footer-widget">
-                        <div class="footer-widget-heading mt-3">
-                            <h3>Links</h3>
-                        </div>
-                        <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/#About">About</a></li>
-                            <li><a href="/#Games">Games</a></li>
-                            <li><a href="#">Faqs</a></li>
-                            <li><a href="#">Store</a></li>
-                            <li><a href="#">Latest News</a></li>
-                            @if (Session::has('user_id'))
-                            <li><a href="#">Support</a></li>
-                            @else
-                            <li><a href="#">Support</a></li>
-                            @endif
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                <div class="footer-widget">
+                    <div class="footer-widget-heading mt-3">
+                        <h3>Links</h3>
+                    </div>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/#About">About</a></li>
+                        <li><a href="/#Games">Games</a></li>
+                        <li><a href="#">Faqs</a></li>
+                        <li><a href="#">Store</a></li>
+                        <li><a href="#">Latest News</a></li>
+                        @if (Session::has('user_id'))
+                        <li><a href="#">Support</a></li>
+                        @else
+                        <li><a href="#">Support</a></li>
+                        @endif
 
-                        </ul>
-                    </div>
+                    </ul>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
-                    <div class="footer-widget">
-                        <div class="footer-widget-heading mt-3">
-                            <h3>Subscribe</h3>
-                        </div>
-                        <div class="footer-text mb-25">
-                            <p>Don't miss to subscribe to our new feeds, kindly fill the form below.</p>
-                        </div>
-                        <div class="subscribe-form">
-                            <form action="#">
-                                <input type="text" placeholder="Email Address">
-                                <button><img src="/img/icons/send.png" style="width: 30px"></button>
-                            </form>
-                        </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                <div class="footer-widget">
+                    <div class="footer-widget-heading mt-3">
+                        <h3>Subscribe</h3>
+                    </div>
+                    <div class="footer-text mb-25">
+                        <p>Don't miss to subscribe to our new feeds, kindly fill the form below.</p>
+                    </div>
+                    <div class="subscribe-form">
+                        <form action="#">
+                            <input type="text" placeholder="Email Address">
+                            <button><img src="/img/icons/send.png" style="width: 30px"></button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <div class="copyright-area">
         <div class="container-fluid">
