9242791bb3c142e0f7897b5fa864a2deb6178aa1
jammmeess <james_bomediano@hotmail.com>
2023-07-20 21:36:59 +0800

    Fixed some Minor Issues

--- resources/views/landing/hero.blade.php
+++ resources/views/landing/hero.blade.php
@@ -1,36 +1,38 @@
-<div class="hero col-lg-12 col-12">
-    <div class="overlay" id="overlay"></div>
-    <img src="/img/landing/HeroBanner.gif" id="hero" class="img-fluid" />
-    <div class="content" id="content">
-        <div class="container">
-            <div class="row mx-5 mt-5 top-0">
+ <div class="container-fluid">
+     <div class="overlay" id="overlay"></div>
+     <div class="hero col-lg-12 col-12">
+         <img src="/img/landing/HeroBanner.gif" id="hero" class="img-fluid" />
+         <div class="content" id="content">
+             <div class="container">
+                 <div class="row mx-5 mt-5 top-0">
 
-                <div class="col-lg-6 col-12 d-none d-sm-block ">
-                    <div class="glitch-wrapper">
-                        <h1 class="fw-bolder display-1">
-                            <div class="glitch" data-text="IGNITE PASSION_">IGNITE PASSION_</div>
-                        </h1>
-                    </div>
-                </div>
+                     <div class="col-lg-6 col-12 d-none d-sm-block ">
+                         <div class="glitch-wrapper">
+                             <h1 class="fw-bolder display-1">
+                                 <div class="glitch" data-text="IGNITE PASSION_">IGNITE PASSION_</div>
+                             </h1>
+                         </div>
+                     </div>
 
-                <div class="col-lg-12 d-none d-sm-block ">
-                    <a href="/store" class="btn text-center" style="font-family: Helmet">Press Start</a>
-                </div>
+                     <div class="col-lg-12 d-none d-sm-block ">
+                         <a href="/store" class="btn text-center" style="font-family: Helmet">Press Start</a>
+                     </div>
 
-                <!-- Responsive -->
-                <div class="col-12 d-block d-sm-none">
-                    <div class="glitch-wrapper-responsive">
-                        <h1 class="fw-bolder">
-                            <div class="glitch-responsive" data-text="IGNITE PASSION_">IGNITE PASSION_</div>
-                        </h1>
-                    </div>
-                </div>
+                     <!-- Responsive -->
+                     <div class="col-12 d-block d-sm-none">
+                         <div class="glitch-wrapper-responsive">
+                             <h1 class="fw-bolder">
+                                 <div class="glitch-responsive" data-text="IGNITE PASSION_">IGNITE PASSION_</div>
+                             </h1>
+                         </div>
+                     </div>
 
-                <div class="col-12 d-block d-sm-none">
-                    <a href="/store" class="btn text-center" style="font-family: Helmet">Press Start</a>
-                </div>
+                     <div class="col-12 d-block d-sm-none">
+                         <a href="/store" class="btn text-center" style="font-family: Helmet">Press Start</a>
+                     </div>
 
-            </div>
-        </div>
-    </div>
-</div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
