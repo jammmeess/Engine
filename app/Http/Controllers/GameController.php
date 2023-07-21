41c635b26775a806555706f0e5dbc730d821bbb9
jammmeess <james_bomediano@hotmail.com>
2023-07-21 19:14:02 +0800

    Proceed to checkout functionality

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -13,91 +13,28 @@ use Session;
 
 class GameController extends Controller
 {
-    // PLACING THE PURCHASE//
-    public function placePurchase(Request $request)
-    {
-        $purchase = new Purchase;
-        $purchase->user_id = Session::get('user_id');
-        $purchase->save();
-
-        $g = Game::query()
-            ->select('game_id', 'game_name', 'price', 'image')
-            ->where('game_id', '>', '0')
-            ->get();
-
-        for ($i = 0; $i < count($g); $i++) {
-            $purchased = $request->input('purchase_' . str($g[$i]->game_id));
-            if ($purchased > 0) {
-                $pg = new PurchasedGame();
-                $pg->purchase_id = $purchase->purchase_id;
-                $pg->game_id = $g[$i]->game_id;
-                $pg->quantity = $purchased;
-                $pg->save();
-            }
-        }
-        return view('usercart', compact('purchase', ''));
-    }
-
-    // SHOWING THE CART//
-    // public function showUserCart()
-    // {
-    //     if (Session::has('user_id')) {
-    //         $cartGames = Session::get('cart', []);
-    //         $total = 0;
 
 
-    //         $g = Game::query()
-    //             ->select('game_id', 'game_name', 'price', 'image_1')
-    //             ->where('game_id', '>', '0')
-    //             ->get();
-
-
-    //         $u = User::query()
-    //             ->select('*')
-    //             ->where(
-    //                 "user_id",
-    //                 "=",
-    //                 Session::get("user_id")
-    //             )
-    //             ->first();
-
-    //         foreach ($cartGames as $cartItem) {
-    //             $total += $cartItem['game']->price * $cartItem['quantity'];
-    //         }
-    //         return view('usercart', compact('cartGames', 'total', 'g', 'u'));
-    //     } else {
-    //         abort(401);
-    //     }
-    // }
-
-
-    // FOR PURCHASING//
     public function makePurchase(Request $request)
     {
-        if (Session::has('user_id')) {
-            $g = Game::query()
-                ->select('game_id', 'game_name', 'price', 'image_1')
-                ->where('game_id', '>', '0')
-                ->get();
+        $u = User::query()
+            ->select('*')
+            ->where("user_id", "=", Session::get("user_id"))
+            ->get()
+            ->first();
 
-            $u = User::query()
-                ->select('*')
-                ->where("user_id", "=", Session::get("user_id"))
-                ->get()
-                ->first();
-
-            $cartGames = array();
-            $total = 0;
-
-            for ($i = 0; $i < count($g); $i++) {
-                if (intval($request->input('purchase_' . str($g[$i]->game_id)))) {
-                    $total += $g[$i]->price->input('purchase_' . str($g[$i]->game_id));
-                }
-                array_push($cartGames, $request->input('purchase_' . str($g[$i]->game_id)));
+        $g = Game::query()
+            ->select('game_id', 'game_name', 'price', 'image_1')
+            ->get();
+        $sg = array();
+        $total = 0;
+        for ($i = 0; $i < count($g); $i++) {
+            if (intval($request->input('purchase_' . str($g[$i]->game_id))) > 0) {
+                $total += $g[$i]->price * $request->input('purchase_' . str($g[$i]->game_id));
             }
-            // return redirect('/userstore')->with(compact('cartGames', 'total', 'g', 'u'))->with('success', 'Game Added to Cart');
-            return view('usercart', compact('g', 'u', 'cartGames', 'total'))->with('Success!', 'Game has been added!');
+            array_push($sg, $request->input('purchase_' . str($g[$i]->game_id)));
         }
+        return view('usercart', compact('g', 'sg', 'total', 'request', 'u'))->with('Success!', 'Game has been added!');
     }
 
 
@@ -107,7 +44,6 @@ class GameController extends Controller
     {
         $games = Game::query()
             ->select(DB::raw('*'))
-            ->where('game_id', '>', '0')
             ->get();
 
         return view('store', compact('games'));
@@ -122,12 +58,11 @@ class GameController extends Controller
                 ->get()
                 ->first();
 
-            $games = Game::query()
+            $game_list = Game::query()
                 ->select(DB::raw('*'))
-                ->where('game_id', '>', '0')
                 ->get();
 
-            return view('userstore', compact('games', 'u'));
+            return view('userstore', compact('game_list', 'u'));
         } else {
             abort(401);
         }
