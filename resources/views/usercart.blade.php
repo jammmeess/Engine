6c8988cb0ec2493b2caa0e03cfec0ae97430ccca
jammmeess <james_bomediano@hotmail.com>
2023-07-21 19:24:06 +0800

    Updated Usercart Look

--- resources/views/usercart.blade.php
+++ resources/views/usercart.blade.php
@@ -2,52 +2,57 @@
 
 <head>
     @include("layouts/head")
-    <title>Shopping Cart</title>
+    <title>Cart</title>
 </head>
 
 <body>
     @include('layouts/navbar')
     @include('layouts/errors')
     <div class="container-fluid my-5">
-        <h3 class="text-light mb-3">Your Cart</h3>
+        <h3 class="text-light ms-5 mb-3"><b>Checkout: </b></h3>
         <form action="#" method="POST">
             @csrf
             <div class="row d-flex">
-                <div class="col-lg-12">
-                    <div class="col-lg-6 justify-content-center align-items-center text-center">
-                        @for($i = 0; $i < count($g); $i++) @if ($pg[$i]>0)
-                            <table class="table table-striped text-light">
+                <div class="col-lg-8 col-10">
+                    <div class="col-lg-12 justify-content-center align-items-center">
+                        @for ($i = 0; $i < count($g); $i++ ) @if ($sg[$i]> 0)
+                            <table class="table table-borderless align-middle table-md text-light ms-5">
                                 <thead>
                                     <tr>
                                         <th></th>
                                         <th>
-                                            <h3>Game Name</h3>
+                                            <h3 class="text-center"><b>Game Name</b></h3>
                                         </th>
                                         <th>
-                                            <h3>Price</h3>
+                                            <h3 class="text-center"><b>Price</b></h3>
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody class="text-light">
-                                    @foreach($g as $games)
+                                <tbody class="text-light ">
                                     <tr>
-                                        <td><img src="/img/games/{{$games->image_1}}" style="width: 40%; border-radius:20px;" /></td>
-                                        <td class="text-light">{{$games->game_name}}</td>
-                                        <td class="text-light">₱{{$games->price}}</td>
+                                        <td class="text-light "><img src="/img/games/{{$g[$i]->image_1}}" alt="{{$g[$i]->game_name}}" /></td>
+                                        <td class="text-light">{{$g[$i]->game_name}}</td>
+                                        <td class="text-light text-center">₱{{$g[$i]->price}}</td>
                                     </tr>
-                                    @endforeach
                                 </tbody>
+                                <tfoot class="table-bordered">
+                                    <tr>
+                                        <td><b>Total:</b> </td>
+                                        <td></td>
+                                        <td class="text-light justify-content-center text-center"><b><i>₱{{ $total }}</i></b></td>
+                                    </tr>
+                                </tfoot>
                             </table>
-                            <div class="col-lg-12 d-flex justify-content-end">
-                                <p class="text-light">Total: ₱{{$total}}</p>
-                            </div>
                             @endif
                             @endfor
                     </div>
-
-                </div>
-                <div class="col-lg-4">
-                    <button type="submit" class="btn">Complete Your Purchase</button>
                 </div>
             </div>
+            <div class="col-lg-8 mt-4 mx-5">
+                <button type="submit" class="btn">Complete Your Purchase</button>
+            </div>
+        </form>
     </div>
+</body>
+
+</html>
