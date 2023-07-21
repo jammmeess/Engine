9877557ca65423c02c166d4285ac282e4c2d4555
jammmeess <james_bomediano@hotmail.com>
2023-07-21 19:21:22 +0800

    Minor Changes

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -1,5 +1,5 @@
 @if (Session::has('success'))
-<p id="notif" style="background-color:  #203c2d; color:white">{{Session::get('success')}}</p>
+<p id="notif" style="background-color:  #203c2d; color:white; margin-right: 10px">{{Session::get('success')}}</p>
 @elseif (Session::has('fail'))
 <p id="notif" style="background-color:  #3e1d1d;color:white">{{Session::get('fail')}}</p>
 @endif
