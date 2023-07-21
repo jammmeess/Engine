48e3095d5eb7163eb8d49f8efc42ce56dbd17712
jammmeess <james_bomediano@hotmail.com>
2023-07-21 19:18:55 +0800

    Scroll Animation

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -1,45 +1,45 @@
 <div class="container-fluid px-5" id="About">
     <div class="row text-light">
         <div class="col-lg-12 d-none d-sm-block">
-            <h3 class="mt-5 display-3 text-light d-flex justify-content-center">About</h3>
+            <h3 class="mt-5 display-3 text-light d-flex justify-content-center fade-in-section">About</h3>
             <div class="line"></div>
         </div>
 
         <div class="col-lg-6 d-none d-sm-block">
-            <h3 class="ms-3 my-5 display-5 text-light about-content">Embark on an incredible Journey with us!</h3>
-            <p class="ms-3 my-3 text-justify text-start text-light about-content">Engine_ emerged from a simple idea and has transformed into an extraordinary gaming ecommerce company. </p>
+            <h3 class="ms-3 my-5 display-5 text-light about-content fade-in-section">Embark on an incredible Journey with us!</h3>
+            <p class="ms-3 my-3 text-justify text-start text-light about-content fade-in-section">Engine_ emerged from a simple idea and has transformed into an extraordinary gaming ecommerce company. </p>
         </div>
 
-        <div class="col-lg-6 d-flex align-items-center d-none d-sm-block pt-1 mt-5">
+        <div class="col-lg-6 d-flex align-items-center d-none d-sm-block pt-1 mt-5 fade-in-section">
             <img src="/img/landing/About.gif" class="img-fluid mt-5 ms-3 " alt="Adventure Awaits" id="about_img" />
         </div>
     </div>
 
     <div class="row">
         <div class="col-lg-6 d-none d-sm-block">
-            <p class="ms-3 my-4 text-justify text-start text-light about-content">Discover a curated collection of games, join a vibrant community, and unleash your passion. With an intuitive interface and exceptional service, we're here to make your gaming dreams a reality. </p>
-            <p class="ms-3 my-3 text-justify text-start text-light about-content">Experience the thrill of Engine_ and embrace unforgettable adventures.</p>
+            <p class="ms-3 my-4 text-justify text-start text-light about-content fade-in-section">Discover a curated collection of games, join a vibrant community, and unleash your passion. With an intuitive interface and exceptional service, we're here to make your gaming dreams a reality. </p>
+            <p class="ms-3 my-3 text-justify text-start text-light about-content fade-in-section">Experience the thrill of Engine_ and embrace unforgettable adventures.</p>
         </div>
     </div>
 
     <!-- Responsive -->
     <div class="row text-light">
         <div class="col-12 d-block d-sm-none justify-text-center text-center">
-            <h3 class="mt-5 display-3 text-light d-flex justify-content-center">About</h3>
+            <h3 class="mt-5 display-3 text-light d-flex justify-content-center fade-in-section">About</h3>
             <div class="line  mb-5"></div>
         </div>
 
         <div class="col-12 d-block d-sm-none justify-text-center text-center">
-            <h3 class="ms-3 my-1 display-6 text-light about-content">Embark on an incredible Journey with us!</h3>
-            <p class="ms-3  text-light about-content">Engine_ emerged from a simple idea and has transformed into an extraordinary gaming ecommerce company. </p>
+            <h3 class="ms-3 my-1 display-6 text-light about-content fade-in-section">Embark on an incredible Journey with us!</h3>
+            <p class="ms-3  text-light about-content fade-in-section">Engine_ emerged from a simple idea and has transformed into an extraordinary gaming ecommerce company. </p>
         </div>
 
         <div class="col-12 d-block d-sm-none justify-text-center text-center">
-            <p class="ms-3  text-light about-content">Discover a curated collection of games, join a vibrant community, and unleash your passion. With an intuitive interface and exceptional service, we're here to make your gaming dreams a reality. </p>
-            <p class="ms-3  text-light about-content">Experience the thrill of Engine_ and embrace unforgettable adventures.</p>
+            <p class="ms-3  text-light about-content fade-in-section">Discover a curated collection of games, join a vibrant community, and unleash your passion. With an intuitive interface and exceptional service, we're here to make your gaming dreams a reality. </p>
+            <p class="ms-3  text-light about-content fade-in-section">Experience the thrill of Engine_ and embrace unforgettable adventures.</p>
         </div>
 
-        <div class="col-12 d-block d-sm-none  d-flex justify-content-center ">
+        <div class="col-12 d-block d-sm-none  d-flex justify-content-center fade-in-section ">
             <img src="/img/landing/About.gif" class="img-fluid" style="width: 80%" alt="Adventure Awaits" id="about_img" />
         </div>
     </div>
