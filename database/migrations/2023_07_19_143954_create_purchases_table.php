efba576b5268bf51103c888fe7d858377c46d9c1
jammmeess <james_bomediano@hotmail.com>
2023-07-21 19:16:15 +0800

    Updated Database Migration Table

--- database/migrations/2023_07_19_143954_create_purchases_table.php
+++ database/migrations/2023_07_19_143954_create_purchases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePurchasesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -14,9 +14,8 @@ class CreatePurchasesTable extends Migration
     public function up()
     {
         Schema::create('purchases', function (Blueprint $table) {
-            $table->integer('purchase_id');
-            $table->timestamp('time_placed')->useCurrent();
-            $table->integer('game_id');
+            $table->integer('purchase_id', true);
+            $table->dateTime('time_placed')->useCurrent();
             $table->integer('user_id');
         });
     }
@@ -30,4 +29,4 @@ class CreatePurchasesTable extends Migration
     {
         Schema::dropIfExists('purchases');
     }
-}
+};
