6ea5535eb209d443faa71b75f689700be2d78252
jammmeess <james_bomediano@hotmail.com>
2023-07-21 19:21:00 +0800

    Minor Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,8 @@
-
 <script>
-    // Add the 'transparent' class to navbar initially
     document.addEventListener('DOMContentLoaded', function() {
         var navbar = document.querySelector('.navbar');
         navbar.classList.add('transparent');
 
-        // Add or remove the 'scrolled' class based on scroll position
         window.addEventListener('scroll', function() {
             if (window.scrollY > 0) {
                 navbar.classList.remove('transparent');
