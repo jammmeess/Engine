755666371d37b3fae9d055d9f61c75ab6db9f19a
jammmeess <james_bomediano@hotmail.com>
2023-07-22 22:26:40 +0800

    Potential SASS file

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,5 +6,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="/js/hideNotif.js"></script>
 <link rel="stylesheet" href="/css/styles.css" />
+<link rel="stylesheet" href="/css/styles.scss" />
 <script src="{{ asset('js/navbar.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
