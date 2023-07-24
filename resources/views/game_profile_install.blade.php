5920c115bac6285bfeb99799f25145fe4ef1e83a
jammmeess <james_bomediano@hotmail.com>
2023-07-24 18:31:52 +0800

    Phone Respponsive

--- resources/views/game_profile_install.blade.php
+++ resources/views/game_profile_install.blade.php
@@ -28,8 +28,45 @@
     <div class="container-fluid mb-5 pb-5 text-light">
         <h1 class="ms-3 mb-2" style="font-family:Neo-latina">{{$g->game_name}}</h1>
         <p class=" ms-4 mb-5">{{$g->developer}}</p>
-        <div class="row">
-            <div class="col-lg-7 mb-5">
+
+        <div class="row d-none d-sm-block">
+            <div class="col-lg-12 me-5">
+                <h2 class="mb-3 ms-3">You Own This Game.</h2>
+                <button class="btn me-3 ms-3 mb-5">Install Game</button>
+                <span><a class="btn mb-5" href="/my_library">Back to your Library</a></span>
+            </div>
+            <div class="col-lg-12 mb-5">
+                <div id="quad" class=" col-lg-7 d-flex flex-wrap">
+                    <figure>
+                        <img src="/img/games/{{$g->image_3}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/img/games/{{$g->image_4}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/img/games/{{$g->image_5}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/img/games/{{$g->image_6}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                </div>
+
+            </div>
+        </div>
+
+
+        <!-- Responsive -->
+        <div class="row d-block d-sm-none">
+            <div class="col-lg-4 me-5">
+                <h2 class="mb-3 text-center">You Own This Game.</h2>
+                <a class="btn mb-5 ms-5" href="/my_library">Back to your Library</a>
+            </div>
+
+            <div class="col-12 mb-5">
                 <div id="quad" class="d-flex flex-wrap">
                     <figure>
                         <img src="/img/games/{{$g->image_3}}" alt="{{$g->game_name}}">
@@ -49,13 +86,9 @@
                     </figure>
                 </div>
             </div>
-            <div class="col-lg-4 me-5">
-                <!-- <label><b>Description:</b></label> -->
-                <h2 class="mb-3">You Own This Game.</h2>
-                <button class="btn mb-3 d-none d-sm-block">Install Game</button>
-                <a class="btn mb-3 d-none d-sm-block" href="/my_library">Back to your Library</a>
-            </div>
+
         </div>
+
     </div>
     @endforeach
     @include ('layouts/footer')
