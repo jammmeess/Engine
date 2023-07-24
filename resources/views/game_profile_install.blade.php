832e811503f58ff086bf973e3230666bc3039824
jammmeess <james_bomediano@hotmail.com>
2023-07-24 17:36:34 +0800

    Game Profile Viewer For Purchased Games

--- resources/views/game_profile_install.blade.php
+++ resources/views/game_profile_install.blade.php
@@ -3,28 +3,60 @@
 <head>
     @include("layouts/head")
     <title>Game Profile</title>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var quadimages = document.querySelectorAll("#quad figure");
+            for (var i = 0; i < quadimages.length; i++) {
+                quadimages[i].addEventListener('click', function() {
+                    this.classList.toggle("expanded");
+                    quad.classList.toggle("full");
+                });
+            }
+        });
+    </script>
 </head>
 
 <body>
     @include('layouts/navbar')
     @include("layouts/errors")
-    <h1>Profile</h1>
     @foreach ($game as $g)
-    <p>Welcome, {{$g -> game_name}}</p>
-    <img src="/img/user_profiles/{{$g -> image_1}}" width="250px" /><br>
-    <form action="/profile/{{$g -> user_id}}/upload_photo" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <input type="file" name="image"></input><br>
-        <input type="submit" class="btn btn-success" value="Upload photo" />
-    </form>
-    <a href="/logout">Logout</a>
-    <h2>Basic Info</h2>
-    <ul>
-        <li>Birthdate: {{$g-> birthdate}}</li>
-        <li>Gender: {{$g-> gender}}</li>
-        <li>Province: {{$g-> province}}</li>
-    </ul>
+
+    <div class="overlay-game" id="overlay"></div>
+    <div class="hero-store col-lg-12 col-12">
+        <img src="/img/games/{{$g -> image_2}}" id="hero-store" />
+    </div>
+    <div class="container-fluid mb-5 pb-5 text-light">
+        <h1 class="ms-3 mb-2" style="font-family:Neo-latina">{{$g->game_name}}</h1>
+        <p class=" ms-4 mb-5">{{$g->developer}}</p>
+        <div class="row">
+            <div class="col-lg-7 mb-5">
+                <div id="quad" class="d-flex flex-wrap">
+                    <figure>
+                        <img src="/img/games/{{$g->image_3}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/img/games/{{$g->image_4}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/img/games/{{$g->image_5}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/img/games/{{$g->image_6}}" alt="{{$g->game_name}}">
+                        <figcaption>{{$g->game_name}}</figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-lg-4 me-5">
+                <!-- <label><b>Description:</b></label> -->
+                <h2 class="mb-3">You Own This Game.</h2>
+                <button class="btn mb-3 d-none d-sm-block">Install Game</button>
+                <a class="btn mb-3 d-none d-sm-block" href="/my_library">Back to your Library</a>
+            </div>
+        </div>
+    </div>
     @endforeach
     @include ('layouts/footer')
 </body>
